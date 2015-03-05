c62beb43cb8ff30099260d61dcd40e613be85413
hui.xia <hui.xia@vipshop.com>
2015-03-05 15:52:05 +0800

    实例2

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strName;
+            Console.Write("please input your name:");
+            strName = Console.ReadLine();
+            Console.WriteLine("hello,{0}!",strName);
+        }
+    }
+}
