98365387bb7f5298469c2dc7c4c03138a4628a4a
hui.xia <hui.xia@vipshop.com>
2015-03-05 17:13:06 +0800

    实例4

---
+++ predefineTypes/predefineTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace predefineTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "this is a string";
+            Console.WriteLine(str);
+            string strCopy = string.Copy(str);
+            Console.WriteLine(strCopy);
+            bool testbool = (str == strCopy);
+            Console.WriteLine(testbool);
+            testbool = ((object)str == (object)strCopy);
+            Console.WriteLine(testbool);
+            float testfloat = 23223.03F;
+            Console.WriteLine(testfloat);
+            double testdouble = 2323.03D;
+            Console.WriteLine(testdouble);
+            testbool = (testfloat == testdouble);
+            Console.WriteLine(testbool);
+            Console.ReadLine();
+        }
+    }
+}
