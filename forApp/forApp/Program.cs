e417579e1a65395c7d4e75662c246cd39b21bb66
hui.xia <hui.xia@vipshop.com>
2015-03-09 15:45:49 +0800

    实例8

---
+++ forApp/forApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace forApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("打印九九乘法表");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}*{1}={2}\t", i, j, i * j);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
