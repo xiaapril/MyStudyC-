5fbfbe39e33eafe27ad2dc5143fce777557dd4c4
hui.xia <hui.xia@vipshop.com>
2015-04-27 17:48:00 +0800

    实例21

---
+++ myFirstWinApp/myFirstWinApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace myFirstWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(31, 50);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(155, 21);
+            this.txt_input.TabIndex = 0;
+            this.txt_input.Text = "朋友，你好！";
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(58, 107);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(101, 30);
+            this.btn_ok.TabIndex = 1;
+            this.btn_ok.Text = "我想说";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 243);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.txt_input);
+            this.Name = "Form1";
+            this.Text = "我的第一个Windows应用程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Button btn_ok;
+    }
+}
+
---
+++ myFirstWinApp/myFirstWinApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace myFirstWinApp
+{
+    //第一个windows应用程序
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txt_input.Text, "hello");
+        }
+
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        [STAThread]
+        static void main()
+        {
+            Application.Run(new Form1());
+        }
+
+    }
+}
