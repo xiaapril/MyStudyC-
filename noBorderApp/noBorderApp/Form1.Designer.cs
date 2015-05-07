9e84541909fc2d51d37af5c6eb1d65f41f59585b
hui.xia <hui.xia@vipshop.com>
2015-05-07 18:48:49 +0800

    实例35

---
+++ noBorderApp/noBorderApp/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace noBorderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(365, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Tomato;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 40);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 437);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ noBorderApp/noBorderApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace noBorderApp
+{
+    public partial class Form1 : Form
+    {
+        static int frmLastWidth = 0;
+        static int frmLastHeight = 0;
+        static int frmWidth;
+        static int frmHeight;
+        static bool frmIsResizing = false;
+
+        Rectangle frmRectangle = new Rectangle();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            frmRectangle.Location = new Point(Left, Right);
+            frmRectangle.Size = new Size(frmWidth, frmHeight);
+            ControlPaint.DrawReversibleFrame(frmRectangle, Color.Empty, System.Windows.Forms.FrameStyle.Thick);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int sizeageX = (MousePosition.X - this.Location.X);
+                int sizeageY = (MousePosition.Y - this.Location.Y);
+
+                if (sizeageX < 120)
+                    sizeageX = 120;
+
+                if (sizeageY < 81)
+                    sizeageY = 81;
+
+                if (frmLastWidth == 0)
+                    frmLastWidth = frmWidth;
+                if (frmLastHeight == 0)
+                    frmLastHeight = frmHeight;
+                if (frmIsResizing)
+                {
+                    frmRectangle.Location = new Point(Left, Top);
+                    frmRectangle.Size = new Size(frmLastWidth, frmLastHeight);
+                }
+
+                frmIsResizing = true;
+
+                ControlPaint.DrawReversibleFrame(frmRectangle, Color.Empty, System.Windows.Forms.FrameStyle.Thick);
+                frmLastHeight = frmHeight;
+                frmLastWidth = frmHeight;
+
+                frmRectangle.Location = new Point(Left, Top);
+                frmRectangle.Size = new Size(frmHeight, frmWidth);
+
+                ControlPaint.DrawReversibleFrame(frmRectangle, Color.Empty, System.Windows.Forms.FrameStyle.Thick);
+            }
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            frmIsResizing = false;
+            frmRectangle.Location = new Point(Left, Top);
+            frmRectangle.Size = new Size(frmWidth, frmHeight);
+            ControlPaint.DrawReversibleFrame(frmRectangle, Color.Empty, System.Windows.Forms.FrameStyle.Thick);
+            this.Width = frmWidth;
+            this.Height = frmHeight;
+        }
+    }
+}
