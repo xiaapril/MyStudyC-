66056acec761614129fcb9a7833ccd978c882750
hui.xia <hui.xia@vipshop.com>
2015-05-07 16:33:13 +0800

    实例34-2（未完成）

---
+++ TrayApp/TrayApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TrayApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_timer = new System.Windows.Forms.Timer(this.components);
+            this.m_contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.m_trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.m_contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(23, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Right click on the System Tray icon for options";
+            // 
+            // m_timer
+            // 
+            this.m_timer.Interval = 150;
+            this.m_timer.Tick += new System.EventHandler(this.m_timer_Tick);
+            // 
+            // m_contextMenuStrip
+            // 
+            this.m_contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItem4,
+            this.menuItem2,
+            this.menuItem1,
+            this.menuItem3});
+            this.m_contextMenuStrip.Name = "m_contextMenuStrip";
+            this.m_contextMenuStrip.Size = new System.Drawing.Size(111, 92);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Name = "menuItem1";
+            this.menuItem1.Size = new System.Drawing.Size(192, 22);
+            this.menuItem1.Text = "&Start";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Name = "menuItem2";
+            this.menuItem2.Size = new System.Drawing.Size(192, 22);
+            this.menuItem2.Text = "about";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Name = "menuItem3";
+            this.menuItem3.Size = new System.Drawing.Size(152, 22);
+            this.menuItem3.Text = "Exit";
+            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Name = "menuItem4";
+            this.menuItem4.Size = new System.Drawing.Size(192, 22);
+            this.menuItem4.Text = "stop";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // m_trayIcon
+            // 
+            this.m_trayIcon.ContextMenuStrip = this.m_contextMenuStrip;
+            this.m_trayIcon.Text = "Animate Tray Icon";
+            this.m_trayIcon.Visible = true;
+            this.m_trayIcon.DoubleClick += new System.EventHandler(this.m_trayIcon_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 262);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animate Tray Icon";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.m_contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer m_timer;
+        private System.Windows.Forms.ContextMenuStrip m_contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem2;
+        private System.Windows.Forms.ToolStripMenuItem menuItem3;
+        private System.Windows.Forms.ToolStripMenuItem menuItem4;
+        private System.Windows.Forms.NotifyIcon m_trayIcon;
+    }
+}
+
---
+++ TrayApp/TrayApp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrayApp
+{
+    public partial class Form1 : Form
+    {
+        private Icon m_Icon1;
+        private Icon m_Icon2;
+        private bool m_bTrayFlag;
+        private bool m_bIconFlag;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_bTrayFlag = false;
+            m_bIconFlag = true;
+
+            menuItem4.Enabled = false;
+
+            try
+            {
+                m_Icon1 = new Icon("Icon1.ico");
+                m_Icon2 = new Icon("Icon2.ico");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error " + e.Message, "Animate Tray - Error");
+                menuItem4.Enabled = false;
+                menuItem1.Enabled = false;
+            }
+        }
+
+        private void m_trayIcon_DoubleClick(object sender, EventArgs e)
+        {
+            if (m_bTrayFlag == true)
+            {
+                this.Activate();
+                this.Show();
+                this.Refresh();
+                m_bTrayFlag = false;
+            }
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            m_timer.Stop();
+            menuItem4.Enabled = false;
+            menuItem1.Enabled = true;
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+           // AboutDlg dlg = new AboutDlg();
+           // dlg.ShowDialog(this);
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            m_timer.Start();
+            menuItem4.Enabled = false;
+            menuItem1.Enabled = true;
+        }
+
+        private void m_timer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuItem3_Click(object sender, EventArgs e)
+        {
+            m_trayIcon.Dispose();
+            this.Close();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
