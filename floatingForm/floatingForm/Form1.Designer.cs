aabbdacbb23a451d41674c4535344843465390c3
hui.xia <hui.xia@vipshop.com>
2015-05-08 15:34:18 +0800

    实例37

---
+++ floatingForm/floatingForm/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace floatingForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(17, 0, 17, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 66);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "圣诞快乐!";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(35F, 66F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 63);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("华文彩云", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.Color.SandyBrown;
+            this.Margin = new System.Windows.Forms.Padding(17, 17, 17, 17);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "漂浮窗口";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ floatingForm/floatingForm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace floatingForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point p = new Point(this.DesktopLocation.X + 1, this.DesktopLocation.Y);
+            this.DesktopLocation = p;
+            if (p.X == this.DesktopBounds.Width)
+            {
+                Point p2 = new Point(0, 370);
+                this.DesktopLocation = p2;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point p = new Point(0, 370);
+            this.DesktopLocation = p;
+        }
+    }
+}
