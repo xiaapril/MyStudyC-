eedc9595eeb293b9b6bc0620f9b015c391d5c3b5
hui.xia <hui.xia@vipshop.com>
2015-05-12 11:31:01 +0800

    实例41

---
+++ diy/diy/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace diy
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            txtBox.Text = "你已经点击了" + count + "次";
+            this.BackColor = Color.IndianRed;
+            txtBox.BackColor = Color.Cyan;
+        }
+    }
+}
