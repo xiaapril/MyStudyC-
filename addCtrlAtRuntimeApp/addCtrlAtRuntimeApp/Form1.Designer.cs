c561b7cf78d7c23e4c5ad6ebad529d2d766d4497
hui.xia <hui.xia@vipshop.com>
2015-05-12 14:55:41 +0800

    实例42

---
+++ addCtrlAtRuntimeApp/addCtrlAtRuntimeApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace addCtrlAtRuntimeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_addButtons = new System.Windows.Forms.Button();
+            this.txt_msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_addButtons
+            // 
+            this.btn_addButtons.Location = new System.Drawing.Point(12, 16);
+            this.btn_addButtons.Name = "btn_addButtons";
+            this.btn_addButtons.Size = new System.Drawing.Size(71, 26);
+            this.btn_addButtons.TabIndex = 0;
+            this.btn_addButtons.Text = "添加按钮";
+            this.btn_addButtons.UseVisualStyleBackColor = true;
+            this.btn_addButtons.Click += new System.EventHandler(this.btn_addButtons_Click);
+            // 
+            // txt_msg
+            // 
+            this.txt_msg.Location = new System.Drawing.Point(100, 19);
+            this.txt_msg.Name = "txt_msg";
+            this.txt_msg.Size = new System.Drawing.Size(172, 21);
+            this.txt_msg.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txt_msg);
+            this.Controls.Add(this.btn_addButtons);
+            this.Name = "Form1";
+            this.Text = "动态添加按钮";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_addButtons;
+        private System.Windows.Forms.TextBox txt_msg;
+    }
+}
+
---
+++ addCtrlAtRuntimeApp/addCtrlAtRuntimeApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace addCtrlAtRuntimeApp
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;//用于计数，添加了多少按钮
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_addButtons_Click(object sender, EventArgs e)
+        {
+            //计数，计算目前是添加的第几个按钮
+            count++;
+
+            //计算待添加按钮的位置
+            int localX = 10 * count;
+            int localY = this.btn_addButtons.Height * count + 20;
+
+            Button toAddButton = new Button();
+
+            //设置按钮属性，记住初始化新添加控件的位置
+            toAddButton.Name = "Button" + count;
+            toAddButton.Text = "按钮" + count + "";
+            toAddButton.Location=new Point(localX, localY);
+
+            //设置添加按钮的控件
+            toAddButton.MouseEnter += new EventHandler(this.btn_MouseEnter);
+            toAddButton.MouseLeave+=new EventHandler(this.btn_MouseLeave);
+            toAddButton.Click+=new EventHandler(this.btn_Click);
+
+            this.Controls.Add(toAddButton);
+        }
+
+        private void btn_MouseEnter(object sender,EventArgs e)
+        {
+            Button currentButton = (Button)sender;
+            currentButton.BackColor = Color.Blue;
+        }
+
+         private void btn_MouseLeave(object sender,EventArgs e)
+        {
+            Button currentButton = (Button)sender;
+            currentButton.BackColor = DefaultBackColor;
+        }
+
+         private void btn_Click(object sender,EventArgs e)
+        {
+            Button currentButton = (Button)sender;
+            txt_msg.Text = "你点击了" + currentButton.Text;
+
+        }
+    }
+}
