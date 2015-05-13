53b74c0b6cb63e700b881e4be66acf9c3266a806
hui.xia <hui.xia@vipshop.com>
2015-05-13 18:21:11 +0800

    实例45

---
+++ htmlMaker/htmlMaker/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace htmlMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.txbFilename = new System.Windows.Forms.TextBox();
+            this.btnGenerateCode = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stbMsg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "文件名";
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(111, 51);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(127, 21);
+            this.txbName.TabIndex = 2;
+            this.txbName.Text = "who a u";
+            // 
+            // txbFilename
+            // 
+            this.txbFilename.Location = new System.Drawing.Point(113, 94);
+            this.txbFilename.Name = "txbFilename";
+            this.txbFilename.Size = new System.Drawing.Size(125, 21);
+            this.txbFilename.TabIndex = 3;
+            this.txbFilename.Text = "who a u.html";
+            // 
+            // btnGenerateCode
+            // 
+            this.btnGenerateCode.Location = new System.Drawing.Point(27, 152);
+            this.btnGenerateCode.Name = "btnGenerateCode";
+            this.btnGenerateCode.Size = new System.Drawing.Size(64, 28);
+            this.btnGenerateCode.TabIndex = 4;
+            this.btnGenerateCode.Text = "生成";
+            this.btnGenerateCode.UseVisualStyleBackColor = true;
+            this.btnGenerateCode.Click += new System.EventHandler(this.btnGenerateCode_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(114, 152);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(65, 29);
+            this.btnClearAll.TabIndex = 5;
+            this.btnClearAll.Text = "清空";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(202, 152);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(68, 28);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stbMsg});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stbMsg
+            // 
+            this.stbMsg.Name = "stbMsg";
+            this.stbMsg.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnGenerateCode);
+            this.Controls.Add(this.txbFilename);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "定制网页";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox txbFilename;
+        private System.Windows.Forms.Button btnGenerateCode;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stbMsg;
+    }
+}
+
---
+++ htmlMaker/htmlMaker/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace htmlMaker
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        private string filename;
+        private StreamWriter writer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGenerateCode_Click(object sender, EventArgs e)
+        {
+            name = txbName.Text.Trim();
+            filename = txbFilename.Text.Trim();
+
+            if ((txbFilename.Text == "") || (txbName.Text == ""))
+            {
+                stbMsg.Text = "请先输入\"姓名\"和\"文件名\"";
+                txbName.Focus();
+            }
+            else
+            {
+                if (GenerateCode())
+                {
+                    stbMsg.Text = "已创建文件" + filename;
+                }
+                else
+                {
+                    stbMsg.Text = "创建文件失败！";
+                }
+            }
+        }
+
+        private bool GenerateCode()
+        {
+            //文件存在时是否覆盖
+            FileInfo f = new FileInfo(filename);
+            if (f.Exists)
+            {
+                DialogResult s = MessageBox.Show("该文件已经存在,是否覆盖？","警告",MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation);
+                if (String.Format("{0}", s) == "Yes")
+                {
+                    f.Delete();
+                }
+                else
+                {
+                    s = MessageBox.Show("请输入不同的文件名！", "警告");
+                    txbFilename.Focus();
+                    return false;
+                }
+            }
+
+            //写文件
+            FileStream outputfile = null;
+            try
+            {
+                outputfile = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(outputfile);
+                writer.BaseStream.Seek(0, SeekOrigin.End);
+                DoWrite("<HTML>");
+                DoWrite("<HEAD>");
+                DoWrite("<TITLE>");
+                DoWrite(name + "介绍");
+                DoWrite("</TITLE>");
+                DoWrite("<HEAD>");
+                DoWrite("<BODY　BGCOLOR='Tan'>");
+                DoWrite("<CENTER>");
+                DoWrite("<BR><BR><H2>欢迎参观"+name+"的页面</H2>");
+                DoWrite("<BR><H4><A HREF='http://www.myclass.com'>校友录首页</A></H4>");
+                DoWrite("</CENTER>");
+                DoWrite("</BODY>");
+                DoWrite("</HTML>");
+                writer.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception GenerateCode =" + ex);
+                stbMsg.Text = "Error";
+                outputfile = null;
+                writer = null;
+                return false;
+            }
+            return true;
+        }
+
+        private void DoWrite(string line)
+        {
+            writer.WriteLine(line);
+            writer.Flush();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            txbName.Text = "";
+            txbFilename.Text = "";
+            stbMsg.Text = "清空所有输入框";
+        }
+    }
+}
