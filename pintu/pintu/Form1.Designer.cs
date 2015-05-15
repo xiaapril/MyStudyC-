b836d44d1e23ab41be10695943bcdc0ec13c5fbe
hui.xia <hui.xia@vipshop.com>
2015-05-15 15:38:23 +0800

    实例50

---
+++ pintu/pintu/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace pintu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b15 = new System.Windows.Forms.Button();
+            this.b16 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b14 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(93, 234);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(179, 19);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "恭喜！！你成功了！！";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl1.Visible = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(31, 203);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(59, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "移动次数";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(117, 203);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(11, 12);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "0";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(57, 23);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(37, 35);
+            this.b11.TabIndex = 3;
+            this.b11.Text = "5";
+            this.b11.UseVisualStyleBackColor = true;
+            // 
+            // b10
+            // 
+            this.b10.Location = new System.Drawing.Point(103, 23);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(37, 35);
+            this.b10.TabIndex = 4;
+            this.b10.Text = "6";
+            this.b10.UseVisualStyleBackColor = true;
+            // 
+            // b13
+            // 
+            this.b13.Location = new System.Drawing.Point(149, 23);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(41, 35);
+            this.b13.TabIndex = 5;
+            this.b13.Text = "3";
+            this.b13.UseVisualStyleBackColor = true;
+            // 
+            // b12
+            // 
+            this.b12.Location = new System.Drawing.Point(200, 23);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(36, 35);
+            this.b12.TabIndex = 6;
+            this.b12.Text = "4";
+            this.b12.UseVisualStyleBackColor = true;
+            // 
+            // b15
+            // 
+            this.b15.Location = new System.Drawing.Point(57, 64);
+            this.b15.Name = "b15";
+            this.b15.Size = new System.Drawing.Size(37, 37);
+            this.b15.TabIndex = 7;
+            this.b15.Text = "1";
+            this.b15.UseVisualStyleBackColor = true;
+            // 
+            // b16
+            // 
+            this.b16.Location = new System.Drawing.Point(103, 64);
+            this.b16.Name = "b16";
+            this.b16.Size = new System.Drawing.Size(37, 37);
+            this.b16.TabIndex = 8;
+            this.b16.Text = "*";
+            this.b16.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(149, 64);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(41, 37);
+            this.b8.TabIndex = 9;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(200, 64);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(36, 37);
+            this.b9.TabIndex = 10;
+            this.b9.Text = "7";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // b14
+            // 
+            this.b14.Location = new System.Drawing.Point(57, 109);
+            this.b14.Name = "b14";
+            this.b14.Size = new System.Drawing.Size(37, 34);
+            this.b14.TabIndex = 11;
+            this.b14.Text = "2";
+            this.b14.UseVisualStyleBackColor = true;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(103, 109);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(37, 34);
+            this.b1.TabIndex = 12;
+            this.b1.Text = "15";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(149, 109);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(41, 34);
+            this.b2.TabIndex = 13;
+            this.b2.Text = "14";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(201, 109);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(35, 34);
+            this.b3.TabIndex = 14;
+            this.b3.Text = "13";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(57, 150);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(37, 33);
+            this.b4.TabIndex = 15;
+            this.b4.Text = "12";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(103, 151);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(37, 32);
+            this.b5.TabIndex = 16;
+            this.b5.Text = "11";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(149, 152);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(41, 31);
+            this.b6.TabIndex = 17;
+            this.b6.Text = "10";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(200, 152);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(36, 31);
+            this.b7.TabIndex = 18;
+            this.b7.Text = "9";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(201, 200);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(42, 21);
+            this.t1.TabIndex = 19;
+            this.t1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.t1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b14);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b16);
+            this.Controls.Add(this.b15);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "拼图游戏";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b15;
+        private System.Windows.Forms.Button b16;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b14;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.TextBox t1;
+    }
+}
+
---
+++ pintu/pintu/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pintu
+{
+    public partial class Form1 : Form
+    {
+        int flag;
+        private int count;
+        private int[] numbers = new int[16];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.numbers[0] = 15;
+            this.numbers[1] = 14;
+            this.numbers[2] = 13;
+            this.numbers[3] = 12;
+            this.numbers[4] = 11;
+            this.numbers[5] = 10;
+            this.numbers[6] = 9;
+            this.numbers[7] = 8;
+            this.numbers[8] = 7;
+            this.numbers[9] = 6;
+            this.numbers[10] = 5;
+            this.numbers[11] = 4;
+            this.numbers[12] = 3;
+            this.numbers[13] = 2;
+            this.numbers[14] = 1;
+            this.numbers[15] = -1;
+            this.count = 0;
+            this.flag = 0;         
+        }
+
+       
+
+        private void assign()
+        {
+            if (numbers[0] != -1)
+                b1.Text = numbers[0].ToString();
+            else
+            {
+                b1.Text = "*";
+                b1.Visible = false;
+            }
+
+            if (numbers[1] != -1)
+                b2.Text = numbers[1].ToString();
+            else
+            {
+                b2.Text = "*";
+                b2.Visible = false;
+            }
+
+            if (numbers[2] != -1)
+                b3.Text = numbers[2].ToString();
+            else
+            {
+                b3.Text = "*";
+                b3.Visible = false;
+            }
+
+            if (numbers[3] != -1)
+                b4.Text = numbers[3].ToString();
+            else
+            {
+                b4.Text = "*";
+                b4.Visible = false;
+            }
+
+            if (numbers[4] != -1)
+                b5.Text = numbers[4].ToString();
+            else
+            {
+                b5.Text = "*";
+                b5.Visible = false;
+            }
+
+            if (numbers[5] != -1)
+                b6.Text = numbers[5].ToString();
+            else
+            {
+                b6.Text = "*";
+                b6.Visible = false;
+            }
+
+            if (numbers[6] != -1)
+                b7.Text = numbers[6].ToString();
+            else
+            {
+                b7.Text = "*";
+                b7.Visible = false;
+            }
+
+            if (numbers[7] != -1)
+                b8.Text = numbers[7].ToString();
+            else
+            {
+                b8.Text = "*";
+                b8.Visible = false;
+            }
+
+            if (numbers[8] != -1)
+                b9.Text = "*";
+            else
+            {
+                b9.Text = "*";
+                b9.Visible = false;
+            }
+
+            if (numbers[9] != -1)
+                b10.Text = numbers[9].ToString();
+            else
+            {
+                b10.Text = "*";
+                b10.Visible = false;
+            }
+
+            if (numbers[10] != -1)
+                b11.Text = numbers[10].ToString();
+            else
+            {
+                b11.Text = "*";
+                b11.Visible = false;
+            }
+
+            if (numbers[11] != -1)
+                b12.Text = numbers[11].ToString();
+            else
+            {
+                b12.Text = "*";
+                b12.Visible = false;
+            }
+
+            if (numbers[12] != -1)
+                b13.Text = numbers[12].ToString();
+            else
+            {
+                b13.Text = "*";
+                b13.Visible = false;
+            }
+
+            if (numbers[13] != -1)
+                b14.Text = numbers[13].ToString();
+            else
+            {
+                b14.Text = "*";
+                b14.Visible = false;
+            }
+
+            if (numbers[14] != -1)
+                b15.Text = numbers[14].ToString();
+            else
+            {
+                b15.Text = "*";
+                b15.Visible = false;
+            }
+
+            if (numbers[15] != -1)
+                b16.Text = numbers[15].ToString();
+            else
+            {
+                b16.Text = "*";
+                b16.Visible=false;
+            }
+                
+        }
+
+        private void DownPress()
+        {
+            int chk;
+            int loc;
+            int swap;
+            loc = locator();
+            if (loc > 11)
+                return;
+            swap = numbers[loc + 4];
+            numbers[loc + 4] = -1;
+            numbers[loc] = swap;
+            MakeVisible();
+            assign();
+            chk = CheckForCompletion();
+            lbl3.Text = count.ToString();
+            if (chk == 0)
+            {
+                count++;
+                lbl3.Text = count.ToString();
+            }
+            if(chk==1)
+            {
+                flag = 1;
+                lbl1.Visible = true;
+            }
+        }
+
+        private void UpPress()
+        {
+            int chk;
+            int loc;
+            int swap;
+            loc = locator();
+            if (loc <= 3)
+                return;
+            swap = numbers[loc - 4];
+            numbers[loc - 4] = -1;
+            numbers[loc] = swap;
+            MakeVisible();
+            assign();
+            chk = CheckForCompletion();
+            if (chk == 0)
+            {
+                count++;
+                lbl3.Text = count.ToString();
+            }
+            else
+            {
+                flag = 1;
+                lbl1.Visible = true;
+            }
+        }
+
+        private void RightPress()
+        {
+            int chk;
+            int loc;
+            int swap;
+            loc = locator();
+            if (((loc + 1) % 4) == 0)
+                return;
+            swap = numbers[loc + 1];
+            numbers[loc + 1] = -1;
+            numbers[loc] = swap;
+            MakeVisible();
+            assign();
+            chk = CheckForCompletion();
+            lbl3.Text = count.ToString();
+            if (chk == 0)
+            {
+                count++;
+                lbl3.Text = count.ToString();
+            }
+            if (chk == 1)
+            {
+                flag = 1;
+                lbl1.Visible = true;
+            }
+        }
+
+        private int CheckForCompletion()
+        {
+            int j;
+
+            for (int i = 0; i <= 15; i++)
+            {
+                if (numbers[i] == -1)
+                    numbers[i] = 16;
+            }
+            for (j = 0; j <= 14; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                    break;
+            }
+            if (j == 15)
+                return 1;
+            else
+            {
+                for (int i = 0; i <= 15; i++)
+                {
+                    if (numbers[i] == 16)
+                        numbers[i] = -1;
+                }
+                return 0;
+            }
+        }
+
+        private void MakeVisible()
+        {
+            b1.Visible = true;
+            b2.Visible = true;
+            b3.Visible = true;
+            b4.Visible = true;
+            b5.Visible = true;
+            b6.Visible = true;
+            b7.Visible = true;
+            b8.Visible = true;
+            b9.Visible = true;
+            b10.Visible = true;
+            b11.Visible = true;
+            b12.Visible = true;
+            b13.Visible = true;
+            b14.Visible = true;
+            b15.Visible = true;
+            b16.Visible = true;
+
+
+        }
+
+        private int locator()
+        {
+            int i;
+            for (i = 0; i <= 15; i++)
+            {
+                if (numbers[i] == -1)
+                    break;
+                else
+                    continue;
+            }
+            return i;
+        }
+
+        private void LeftPress()
+        {
+            int chk;
+            int loc;
+            int swap;
+            loc = locator();
+            if ((loc % 4) == 0)
+                return;
+            swap = numbers[loc - 1];
+            numbers[loc - 1] = -1;
+            numbers[loc] = swap;
+            MakeVisible();
+            assign();
+            chk = CheckForCompletion();
+            lbl3.Text = count.ToString();
+            if (chk == 0)
+            {
+                count++;
+                lbl3.Text = count.ToString();
+            }
+            if (chk == 1)
+            {
+                flag = 1;
+                lbl1.Visible = true;
+            }
+        }
+
+        private void t1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (flag == 0)
+            {
+                string keyId = e.KeyCode.ToString();
+                switch (keyId)
+                {
+                    case "Right":
+                        LeftPress();
+                        break;
+                    case "Left":
+                        RightPress();
+                        break;
+                    case "Down":
+                        UpPress();
+                        break;
+                    case "Up":
+                        DownPress();
+                        break;
+                }
+            }
+        }
+    }
+}
